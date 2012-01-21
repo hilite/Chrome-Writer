9e1768a6dee71325a243150cbed8edb0122f3820
hi-lite <hi.lite.g@gmail.com>
2012-01-21 23:18:57 +0900

    fix trim

--- js/popup.js
+++ js/popup.js
@@ -9,14 +9,12 @@ $(function(){
 
 	});
 
-
 	// Button Text to Form 
 	$("#txt2form").click(function () {
 
 		// get localstorage data
 		var splitter = getSpliter();
 		var tagNames = getTagsName().split("\n");
-
 		for(i = 0; i < tagNames.length; i++){
 			if(tagNames[i] == ""){
 				tagNames.splice(i,1);
@@ -24,21 +22,20 @@ $(function(){
 			}
 		}
 
-
 		// get article data
 		var article = document.getElementsByName('article')[0].value.split(splitter);
-
+		var a="";
 		for(var i=0; i < tagNames.length; i++) {	
 			js = "document.getElementsByName('" + tagNames[i] + "')[0].value=''";
 			chrome.tabs.executeScript(null, {code:js});
-			js = "document.getElementsByName('" + tagNames[i] + "')[0].value='" + trim(article[i]).replace(/\r?\n|\r/g, "\\n") + "'";
+			js = "document.getElementsByName('" + tagNames[i] + "')[0].value='" + jQuery.trim(article[i]).replace(/\r?\n|\r/g, "\\n") + "'";
 			chrome.tabs.executeScript(null, {code:js});
 		}
 		//chrome.tabs.executeScript(null, {code:"document.body.style.backgroundColor='red'"});
 
 		window.close();
-	});
 
+	});
 
 });
 
